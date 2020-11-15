dd17fc74306580e49d4e7098015d5634162dcda3
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-15 08:20:38 -0800

    Added initial dom elements and click event to JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var pastTimeBlock = document.querySelector(".past");
+var presentTimeBlock = document.querySelector(".present");
+var futureTimeBlock = document.querySelector(".future");
+
+$(".saveBtn").on("click") {
+
+    localStorage.getItem("schedule9am", "")
+    
+}
