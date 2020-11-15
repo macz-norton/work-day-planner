7f10a4f812eb20549c2faf12f899e217077d7cb5
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-15 15:49:45 -0800

    Added pseudocode

--- script.js
+++ script.js
@@ -2,8 +2,61 @@ var pastTimeBlock = document.querySelector(".past");
 var presentTimeBlock = document.querySelector(".present");
 var futureTimeBlock = document.querySelector(".future");
 
-$(".saveBtn").on("click") {
+
+// moment().format("MMM Do YY") 
+
+// get and store current hour and day from moment.js
+// get time and format method or other methods to fetch the time
+
+// compare current hour of day to numbers that we're looping over
+// less than the current time and day
+// equal to the current time and day
+// greater than the current time and day
+
+// 9 and 17:00 hours in a for loop
+
+// loop over elements to determine time of day
+
+// if past, set to past class
+// if present, set to present class
+// if future, set to future class
+
+// get current hour of the day via momentJS
+
+// loop oer the hours of the day from 9 to 17
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var = 9;
+element ID = "#hour" + i
+
+for (var i = 9; i <= 17; i++) {
+    if ( < currentHour){
+
+        // Select matching element 
+        $(Element ID) past class
+
+    } else if (9am = currentHour) {
+
+
+    } else {
+
+    }
+}
+
+// save inputs in local storage
+// take out information in local storage
+// listens to click events on all buttons
+$("main").on("click") {
 
     localStorage.getItem("schedule9am", "")
-    
+
 }
+
+// save object in an array to save in one key
+// JSON.stringify()
+// JSON.parse()
+
+//OR save every hour in its own key
+
+// go to click button to the same ID
+// dom traversal, cliked button bubbling up, access from event.target
+// how to go to access ID then use as value key to save correct value to correct storage
