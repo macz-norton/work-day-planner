76463af64b1ed2bfb31b805a13306b9503842290
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-21 07:51:17 -0800

    Removed console logs

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 // Display the current date at the top of the scheduler
 var today = moment();
-// Add the current day text to the `currentDay` id
 $("#currentDay").text(today.format('dddd, MMMM Do'));
-console.log(today);
+
 // Set a global variable for the `currentHour`
 var currentHour;
-// Determine the current hour of the day
-// function currentHour() {
-
 currentHour = moment().hour();
 
-// }
-
-// // Determine the current hour and update text area class
+// Determine the current hour and update text area class
 
 for (var i = 9; i < 18; i++) {
     // For past timeblocks
-    console.log("The time: " + i);
     if (i < currentHour) {
 
         $("#hour-" + i).addClass("past");
@@ -36,32 +29,33 @@ for (var i = 9; i < 18; i++) {
 }
 
 
-// $("saveBtn").on("click", function(event){
+$("saveBtn").on("click", function(event){
 
-//     event.preventDefault();
+    event.preventDefault();
 
-//     var scheduleInfo = {
-//         savedHour: $(".scheduleInfo").attr("id"),
-//         userInput: $(".scheduleInfo").val()
-//     }
+    var scheduleInfo = {
+        savedHour: $(".scheduleInfo").attr("id"),
+        userInput: $(".scheduleInfo").val()
+    }
 
-//     console.log(scheduleInfo);
+    console.log(scheduleInfo);
 
-//     scheduleData = JSON.stringify(scheduleInfo);
+    scheduleData = JSON.stringify(scheduleInfo);
 
-//     localStorage.setItem("workSchedule", scheduleData);
+    localStorage.setItem("workSchedule", scheduleData);
 
-// });
+    JSON.parse(scheduleInfo)
 
-// JSON.parse(scheduleInfo)
+});
 
-// document.onload = function() {
 
-//     for (var i = 9; i < 18; i++) {
-//         $("textarea").localStorage.getItem("hour-" + i);
-//     }
+document.onload = function() {
 
-// }
+    for (var i = 9; i < 18; i++) {
+        $("textarea").localStorage.getItem("hour-" + i);
+    }
+
+}
 
 // dom traversal, cliked button bubbling up, access from event.target
 // how to go to access ID then use as value key to save correct value to correct storage
