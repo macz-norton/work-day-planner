95a82685be51d899ba2a1e369693a01454937168
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-21 07:47:53 -0800

    Fixed issue with for loop

--- script.js
+++ script.js
@@ -1,78 +1,67 @@
 // Display the current date at the top of the scheduler
-function displayToday() {
-
-    var today = moment();
-    // Add the current day text to the `currentDay` id
-    $("#currentDay").text(today.format('dddd, MMMM Do'));
-
-}
-
+var today = moment();
+// Add the current day text to the `currentDay` id
+$("#currentDay").text(today.format('dddd, MMMM Do'));
+console.log(today);
 // Set a global variable for the `currentHour`
 var currentHour;
-
 // Determine the current hour of the day
-function currentHour() {
+// function currentHour() {
 
-    currentHour = moment().hour();
+currentHour = moment().hour();
 
-}
+// }
 
-displayToday();
-currentHour();
-
-// Determine the current hour and update text area class
-function checkHour() {
-
-    for (var i = 9; i < 18; i++) {
-        // For past timeblocks
-        if (i < currentHour) {
-
-            $("#hour-" + i).addClass("past");
-
-        // For present timeblocks
-        } else if (i = currentHour) {
-
-            $("#hour-" + i).addClass("present");
-
-        // For future timeblocks
-        } else {
-
-            $("#hour-" + i).addClass("future");
-
-        }
-        // get value out of local storage and assign to text area
-    }
-}
-
-checkHour();
-
-$("saveBtn").on("click", function(event){
-
-    event.preventDefault();
-
-    var scheduleInfo = {
-        savedHour: $(".scheduleInfo").attr("id"),
-        userInput: $(".scheduleInfo").val()
-    }
-
-    console.log(scheduleInfo);
-
-    scheduleData = JSON.stringify(scheduleInfo);
-
-    localStorage.setItem("workSchedule", scheduleData);
-
-});
+// // Determine the current hour and update text area class
 
 for (var i = 9; i < 18; i++) {
-    $("textarea").val(localStorage.getItem("hour-" + i));
+    // For past timeblocks
+    console.log("The time: " + i);
+    if (i < currentHour) {
+
+        $("#hour-" + i).addClass("past");
+
+    // For present timeblocks
+    } else if (i === currentHour) {
+
+        $("#hour-" + i).addClass("present");
+
+    // For future timeblocks
+    } else {
+
+        $("#hour-" + i).addClass("future");
+
+    }
+    // get value out of local storage and assign to text area
 }
 
-// save object in an array to save in one key
-// JSON.stringify()
-// JSON.parse()
 
-// OR save every hour in its own key
+// $("saveBtn").on("click", function(event){
 
-// go to click button to the same ID
+//     event.preventDefault();
+
+//     var scheduleInfo = {
+//         savedHour: $(".scheduleInfo").attr("id"),
+//         userInput: $(".scheduleInfo").val()
+//     }
+
+//     console.log(scheduleInfo);
+
+//     scheduleData = JSON.stringify(scheduleInfo);
+
+//     localStorage.setItem("workSchedule", scheduleData);
+
+// });
+
+// JSON.parse(scheduleInfo)
+
+// document.onload = function() {
+
+//     for (var i = 9; i < 18; i++) {
+//         $("textarea").localStorage.getItem("hour-" + i);
+//     }
+
+// }
+
 // dom traversal, cliked button bubbling up, access from event.target
 // how to go to access ID then use as value key to save correct value to correct storage
